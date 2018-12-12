1b742d28adb061b3064f22aed723b9abbda18a20
Mx70lit <45827637+Mx70lit@users.noreply.github.com>
2018-12-13 00:24:32 +0700

    Add files via upload

---
+++ spam.sh
@@ -0,0 +1,105 @@
+#No recode bangsatt
+#fakk you
+#thanks to Darkmyth Cyber Team
+clear
+
+# Variables
+
+b='\033[1m'
+
+u='\033[4m'
+
+bl='\E[30m'
+
+r='\E[31m'
+
+g='\E[32m'
+
+bu='\E[34m'
+
+m='\E[35m'
+
+c='\E[36m'
+
+w='\E[37m'
+
+endc='\E[0m'
+
+enda='\033[0m'
+
+blue='\e[1;34m'
+
+cyan='\e[1;36m'
+
+red='\e[1;31m'
+
+echo "##################################"|lolcat
+
+echo "/ TOOLS : SPAM v1 /"|lolcat
+
+echo "/ CODED : Mx70lit /"|lolcat
+
+echo "/ From Darkmyths Cyber Team /"|lolcat
+
+echo "/ IG : @Medhat_70lit /"|lolcat
+
+echo "##################################"|lolcat
+
+
+echo "daftar tools spam"
+
+echo "[1] Bom Chat Whatsapp"
+
+echo "[2] Spam Telpon"
+
+echo "[3] Keluar"
+
+read -p "PILIH Satu aja cuk jan banyak banyak : " tolit
+
+
+if [ $tolit = 1 ] || [ $tolit = 01 ]
+
+then
+
+clear
+
+php bomwa.php
+
+sh spam.sh
+
+fi
+
+
+if [ $tolit = 2 ] || [ $tolit = 01 ]
+
+then
+
+clear
+
+php telpon.php
+
+sh spam.sh
+
+fi
+
+
+if [ $bro = 3 ] || [ $bro = 03 ]
+
+then
+
+echo "\033[31;1m We Are Anoymous"
+
+sleep 1
+
+echo "\033[32;1m Tengkyuu cuk"
+
+sleep 1
+
+echo "\033[35;1m done cukk,"
+
+sleep 1
+
+exit
+
+fi
+
